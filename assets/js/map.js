870cb6a400d7431175c7a22df7d233f0362adc75
Jack <jack200034@hotmail.com>
2020-10-28 19:32:46 +0000

    Added markers

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,71 @@
 function initMap(){
+    // Map options
     var options = {
-        zoom: 4,
-        center: {lat: 35.8617, lng:104.1954}
+      zoom:4,
+      center:{lat:35.8617,lng:104.1954}
     }
 
+    // New map
     var map = new google.maps.Map(document.getElementById('map'), options);
-}
+
+    // Listen for click on map
+    google.maps.event.addListener(map, 'click', function(event){
+      // Add marker
+      addMarker({coords:event.latLng});
+    });
+
+    new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+
+    // Array of markers
+    var markers = [
+      {
+        coords:{lat:31.2403,lng:121.4906},
+        iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        content:'<h1></h1>'
+      },
+      {
+        coords:{lat:31.2272,lng:121.4921},
+        content:'<h1></h1>'
+      },
+      {
+        coords:{lat:31.2335,lng:121.5056}
+      }
+    ];
+
+    // Loop through markers
+    for(var i = 0;i < markers.length;i++){
+      // Add marker
+      addMarker(markers[i]);
+    }
+
+    // Add Marker Function
+    function addMarker(props){
+      var marker = new google.maps.Marker({
+        position:props.coords,
+        map:map,
+        //icon:props.iconImage
+      });
+
+      // Check for customicon
+      if(props.iconImage){
+        // Set icon image
+        marker.setIcon(props.iconImage);
+      }
+
+      // Check content
+      if(props.content){
+        var infoWindow = new google.maps.InfoWindow({
+          content:props.content
+        });
+
+        marker.addListener('click', function(){
+          infoWindow.open(map, marker);
+        });
+      }
+    }
+  }
+
+  
