3e8693a6124f8de0cbe51242d5934af4d3991458
Jack <jack200034@hotmail.com>
2020-10-28 21:19:10 +0000

    Formatted and added comments

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,24 +1,25 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: { lat: 35.8617, lng: 104.1954 },
   });
 
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  const markers = locations.map((location, i) => {
+  var markers = locations.map((location, i) => {
     return new google.maps.Marker({
       position: location,
       label: labels[i % labels.length],
     });
   });
-
+// Clusters
   new MarkerClusterer(map, markers, {
     imagePath:
       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
-const locations = [
+// Locations
+var locations = [
   {lat:31.2403,lng:121.4906},
   {lat:31.2272,lng:121.4921},
   {lat:31.2335,lng:121.5056},
