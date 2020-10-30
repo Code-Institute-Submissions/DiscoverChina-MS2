f699738e8c6d2243496e5c91cd2f5bba09f92398
JackTubby <jack200034@hotmail.com>
2020-10-30 16:17:43 +0000

    Rewrote script so multiple info windows work

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,28 +1,65 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: 35.8617, lng: 104.1954 },
+function initMap(){
+  // Map options
+  var options = {
+    zoom:4,
+    center: { lat: 35.8617, lng: 104.1954 }
+  }
+
+  // New map
+  var map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Listen for click on map
+  google.maps.event.addListener(map, 'click', function(event){
   });
+  // Array of markers
+  var markers = [
+    {
+      coords:{lat:31.2403,lng:121.4906},
+      iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+      content:'<h1>Test 1</h1>'
+    },
+    {
+      coords:{lat:31.2272,lng:121.4921},
+      content:'<h1>Test 2</h1>'
+    },
+    {
+      coords:{lat:31.2335,lng:121.5056},
+      content: '<p>Hello World</p>'
+    },
+    {
+      coords:{lat:31.6762,lng:121.6503}
+    }
+  ];
 
-  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Loop through markers
+  for(var i = 0;i < markers.length;i++){
+    // Add marker
+    addMarker(markers[i]);
+  }
 
-  var markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
+  // Add Marker Function
+  function addMarker(props){
+    var marker = new google.maps.Marker({
+      position:props.coords,
+      map:map,
+      //icon:props.iconImage
     });
-  });
-// Clusters
-  new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-  });
-}
-// Locations
-var locations = [
-  {lat:31.2403,lng:121.4906},
-  {lat:31.2272,lng:121.4921},
-  {lat:31.2335,lng:121.5056},
-  {lat:31.6762,lng:121.6503}
 
-];
+    // Check for customicon
+    if(props.iconImage){
+      // Set icon image
+      marker.setIcon(props.iconImage);
+    }
+
+    // Check content
+    if(props.content){
+      var infoWindow = new google.maps.InfoWindow({
+        content:props.content
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+}
