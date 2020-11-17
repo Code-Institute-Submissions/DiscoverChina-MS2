e484f838f01f8c58b14ee5dad6bccde00a6de38b
JackTubby <jack200034@hotmail.com>
2020-11-17 20:26:40 +0000

    Added markers/ info windows into maps

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,23 +1,28 @@
 // array of lat and lng locations
 let locations = [
+    // Location China
     {
-      lat:-33.91722,
-      lng:151.23064
+      lat:35.8593,
+      lng:104.1361
     },
+    // Location Beijing
     {
-    lat:22.3193,
-    lng:114.1694
+    lat:39.916668,
+    lng:116.383331
     },
+    // Location Shanghai
     {
-    lat:22.3255,
-    lng:114.3532
+    lat:31.224361,
+    lng:121.469170
     },
+    // Location Hong Kong
     {
-    lat:23.3532,
-    lng:115.1234
+    lat:22.302711,
+    lng:114.177216
     }
 ];
 let elements = ['maps', 'map-rec-one', 'map-rec-two', 'map-rec-three']; // array of html element ids
+
 
 window.addEventListener('load', 
 function() {
@@ -31,6 +36,60 @@ function initMap(location, element) {
     console.log(location)
     console.log(element)
     map = new google.maps.Map(document.getElementById(element), {zoom:4, center: location});
+
+    // Listen for click on map
+  google.maps.event.addListener(map, 'click', function(event){
+  });
+  // Array of markers
+  var markers = [
+    {
+      coords:{lat:31.2403,lng:121.4906},
+      iconImage:'assets/images/marker.png',
+      content:'<h2>Shanghai</h2>'
+    },
+    {
+      coords:{lat:39.9042,lng:116.4074},
+      iconImage:'assets/images/marker.png',
+      content:'<h2>Beijing</h2>'
+    },
+    {
+      coords:{lat:22.3193,lng:114.1694},
+      iconImage:'assets/images/marker.png',
+      content: '<h2>Hong Kong</h2>'
+    }
+  ];
+
+  // Loop through markers
+  for(var i = 0;i < markers.length;i++){
+    // Add marker
+    addMarker(markers[i]);
+  }
+
+  // Add Marker Function
+  function addMarker(props){
+    var marker = new google.maps.Marker({
+      position:props.coords,
+      map:map,
+      //icon:props.iconImage
+    });
+
+    // Check for customicon
+    if(props.iconImage){
+      // Set icon image 
+      marker.setIcon(props.iconImage);
+    }
+
+    // Check content
+    if(props.content){
+      var infoWindow = new google.maps.InfoWindow({
+        content:props.content
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+    }
+  }
 }
 
 // window.addEventListener('load', initMap())
@@ -51,7 +110,7 @@ function initMap(location, element) {
 function initMap(){
   // Map options
   var options = {
-    zoom:4,
+    zoom:4, 
     center: { lat: 35.8617, lng: 104.1954 }
   }
 
