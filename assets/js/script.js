5c4bbf05378a310b0017ae96acb097e013ea7eb3
Jack <jack200034@hotmail.com>
2020-12-16 13:01:06 +0000

    Removed modal script added in new modal script

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,31 +68,18 @@ function chooseCategorie(chosen) {
   
     document.getElementById(chosen).style.display = "block";
   }
+  
+ /* Modal */
 
-  /* Modal */
-
-//// Modals Script //// 
-//// Get the modal Image 1
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-} 
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
+ var infoImages = document.querySelectorAll(".info-img");
+ for ( i = 0; i < infoImages.length; i++) {
+   document.getElementsByClassName("info-img").addEventListener("click", );
+ }
+ 
+ infoImages.onClick = function() {
+     
+ }
+  
 
 
 
