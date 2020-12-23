12ec77fcbcc07156a749234d2ee9f19ac6ba49c3
Jack <jack200034@hotmail.com>
2020-12-23 14:36:28 +0000

    Removed hero/slideshow script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,3 @@
-// Auto hero image / slideshow
-var i = 0; 
-var images = [];
-var time = 3000;
-
-images[0] = 'assets/images/shanghaiSlideImg.jpg';
-images[1] = 'assets/images/beijingSlideImg.jpg';
-images[2] = 'assets/images/hongKongSlideImg.jpg';
-
-function changeImg(){
-    document.slide.src = images[i];
-
-    if(i < images.length - 1){
-        i++;
-    } else {
-        i = 0;
-    }
-
-    setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
-
 // More info China btn
 function factsChina() {
   var factsChina = document.getElementById("factsChinaHidden");
